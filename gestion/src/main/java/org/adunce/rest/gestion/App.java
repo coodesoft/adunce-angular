c8872f673d6d65d8dbb4e3369bbb50b9df1cf2f5
mgiamberardino <mgiamberardino@gmail.com>
2015-03-29 20:51:05 -0300

    Desplegable

--- gestion/src/main/java/org/adunce/rest/gestion/App.java
+++ gestion/src/main/java/org/adunce/rest/gestion/App.java
@@ -2,6 +2,8 @@ package org.adunce.rest.gestion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 import com.mangofactory.swagger.plugin.EnableSwagger;
 
@@ -11,7 +13,7 @@ import com.mangofactory.swagger.plugin.EnableSwagger;
  */
 @SpringBootApplication
 @EnableSwagger
-public class App
+public class App extends SpringBootServletInitializer
 {
 	
 	public static final String RESOURCE_ID = "blog_resource";
@@ -20,5 +22,10 @@ public class App
     public static void main( String[] args )
     {
         SpringApplication.run(App.class);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
     }
 }
