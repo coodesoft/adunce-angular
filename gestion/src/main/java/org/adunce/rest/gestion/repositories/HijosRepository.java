a84c7e33a1a86a73f740667fd1905c99493bb3cc
Mauro J Giamberardino <mgiamberardino@tecnisys.com>
2015-09-29 17:17:29 -0300

    Add HijosRepository

---
+++ gestion/src/main/java/org/adunce/rest/gestion/repositories/HijosRepository.java
@@ -0,0 +1,8 @@
+package org.adunce.rest.gestion.repositories;
+
+import org.adunce.rest.gestion.model.Hijo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HijosRepository extends JpaRepository<Hijo, String> {
+
+}
