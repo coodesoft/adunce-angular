d5c64279bfe6204852e3437dd24fa0fb5c6dab87
Leonardo Navarro <maikndawer@gmail.com>
2015-10-23 17:49:44 -0300

    se añadió el metodo PUT

--- gestion/src/main/java/org/adunce/rest/gestion/filters/CORSFilter.java
+++ gestion/src/main/java/org/adunce/rest/gestion/filters/CORSFilter.java
@@ -22,7 +22,7 @@ public class CORSFilter implements Filter{
 		HttpServletResponse response = (HttpServletResponse) res;
 			
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		//response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
 		response.setHeader("Access-Control-Allow-Headers", "Content-Type,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
